64e6b25e25d2f639b9fa0ce122e3d7f55622d1fc
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-16 19:54:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzBuzz(number)
+def fizzbuzz(number)
   if number % 3 == 0 && number / 5 == 0
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elsif number % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif number % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   else
     return nil
   end
